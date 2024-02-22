0a2d50da22a8e5508cdc6931797cd23a5893d667
Nitish1604 <nitsprasad03@gmail.com>
2024-02-22 14:53:50 +0530

    second-commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -78,8 +78,8 @@ export default function Footer() {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
             © 2023
-            <a href="https://hiteshchoudhary.com/" className="hover:underline">
-              hiteshchoudhary
+            <a href="https://github.com/Nitish1604" className="hover:underline">
+              Nitish
             </a>
             . All Rights Reserved.
           </span>
